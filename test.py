7edb376b8b6cfcbd16b2506ceacbc38d20ce4b99
Khushal Sahni <kstheking0@gmail.com>
2021-12-06 08:41:56 +0530

    initial

---
+++ test.py
@@ -0,0 +1,5 @@
+from tkinter import *
+root = Tk() # root widget
+testLabel = Label(root, text = "Hey bitch!") # where u want this label to be
+testLabel.pack() # pack this label 
+root.mainloop() # the main loop of the root widget
